5009269e707ab96a8516f67c8c71f8bd6d6042a7
張祐禎 <s27840213@gmail.com>
2023-03-28 23:31:55 +0800

    set up the project

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <h6 class="bg">12345</h6>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
@@ -20,8 +21,5 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
